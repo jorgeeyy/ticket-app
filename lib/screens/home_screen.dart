5303c27a262e738592866e752e5379440419bfa3
JorgeQ1 <jqubez@gmail.com>
2025-02-25 10:00:47 +0000

    homescreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
+
+import '../base/res/media.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Good morning",
+                              style: AppStyles.headLineStyle3
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              "Book Tickets",
+                              style: AppStyles.headLineStyle1
+                            )
+                          ],
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(image: AssetImage(AppMedia.logo))
+
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 25,),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        color: const Color(0x0ff4f6fd),
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                          Text("Search icon")],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
